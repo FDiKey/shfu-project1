2217cdddc2465a98e95fb51d14cf297ca39fa5b5
Kirill Malyshev <fun.d.kira@gmail.com>
2018-06-16 11:44:28 +0300

    first commit

--- app/src/main/java/com/examplex/kirill/project1/MainActivity.java
+++ app/src/main/java/com/examplex/kirill/project1/MainActivity.java
@@ -1,5 +1,7 @@
 package com.examplex.kirill.project1;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splach);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3*1000);
+
     }
 }
